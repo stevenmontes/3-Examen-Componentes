f616f11b208687320c4315a1a611231828ce461c
Steven Montes <cmontesa@ucenfotec.ac.cr>
2019-08-16 20:10:08 -0600

    Agrega metodo Buscar provincias por aproximación del nombre

--- src/main/java/com/cenfotec/examen3/controller/DivisionPoliticaController.java
+++ src/main/java/com/cenfotec/examen3/controller/DivisionPoliticaController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cenfotec.examen3.domain.DivisionPolitica;
@@ -24,35 +25,39 @@ public class DivisionPoliticaController {
 
 	@Autowired
 	private DivisionPoliticaRepository repo;
-	
+
 	@PostMapping
 	public DivisionPolitica Create(@Valid @RequestBody DivisionPolitica nDivPolitica) {
 		return repo.save(nDivPolitica);
 	}
-	
+
 	@GetMapping
-	public List<DivisionPolitica> GetAll(){
+	public List<DivisionPolitica> GetAll(@RequestParam(value = "nombre", required = false) String nombre) {
+		if(nombre != null) {
+			return repo.findAllByNombreContaining(nombre);
+		}
+		
 		return repo.findAll();
 	}
-	
+
 	@GetMapping("/{nombre}")
-	public Optional<DivisionPolitica> Get(@PathVariable(value = "nombre") String nombre){
+	public Optional<DivisionPolitica> Get(@PathVariable(value = "nombre") String nombre) {
 		return repo.findById(nombre);
 	}
-	
+
 	@PutMapping
 	public DivisionPolitica Update(@Valid @RequestBody DivisionPolitica nDivPolitica) {
 		return repo.save(nDivPolitica);
 	}
-	
+
 	@DeleteMapping("/{nombre}")
-	public Optional<DivisionPolitica> Delete(@PathVariable(value = "nombre") String nombre){
+	public Optional<DivisionPolitica> Delete(@PathVariable(value = "nombre") String nombre) {
 		Optional<DivisionPolitica> divPolitica = repo.findById(nombre);
-		
-		if(divPolitica != null) {
+
+		if (divPolitica != null) {
 			repo.deleteById(nombre);
 		}
-		
+
 		return divPolitica;
 	}
 }
--- src/main/java/com/cenfotec/examen3/repository/DivisionPoliticaRepository.java
+++ src/main/java/com/cenfotec/examen3/repository/DivisionPoliticaRepository.java
@@ -1,9 +1,15 @@
 package com.cenfotec.examen3.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cenfotec.examen3.domain.DivisionPolitica;
 
-public interface DivisionPoliticaRepository extends JpaRepository<DivisionPolitica, String>{
-	
+public interface DivisionPoliticaRepository extends JpaRepository<DivisionPolitica, String> {
+
+	@Query("select div from DivisionPolitica div where div.Nombre like %?1%")
+	List<DivisionPolitica> findAllByNombreContaining(String nombre);
+
 }
